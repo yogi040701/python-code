b5aee767a82815554cc28309bb34d0d0794e33b6
yogi040701 <76053563+yogi040701@users.noreply.github.com>
2025-09-10 11:06:24 +0530

    Add files via upload

---
+++ Assignment_2/task_1.py
@@ -0,0 +1,9 @@
+# ASSIGNMENT 2:
+# Module 3: Control Structures in Python
+# Task 1: Check if a Number is Even or Odd
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num, "is an even number.")
+else:
+    print(num, "is an odd number.")
